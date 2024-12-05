c81908b09c8c2620fbaf01c2e9c860f5c2972c6a
unknown <rex.okland@gmail.com>
2024-12-05 16:03:19 -0700

    adding in new middleware stuff

--- src/service.js
+++ src/service.js
@@ -5,6 +5,9 @@ const franchiseRouter = require('./routes/franchiseRouter.js');
 const version = require('./version.json');
 const config = require('./config.js');
 
+//METRICS FOR GRAFANA//
+const metrics = require('./metrics.js');
+
 const app = express();
 app.use(express.json());
 app.use(setAuthUser);
@@ -13,6 +16,13 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+//req 1//
+app.use((req, res, next) => {
+  if(req.method == 'POST'){metrics.incrementPostRequests}
+  else if(req.method == 'GET'){metrics.incrementGetRequests}
+  else if(req.method == 'DELETE'){metrics.incrementDeleteRequests}
   next();
 });
 
